a6c461da331db016db222b863c561de3726b097f
Gert-Jan Schokkaert <gert-jan.schokkaert@ordina.be>
2020-05-24 14:09:06 +0200

    added socket.io

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from "cors";
+import socketIo from "socket.io";
+import http from "http";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import postOrder from "./controllers/postOrder";
@@ -12,7 +15,12 @@ const connectionString = process.env.CONNECTION_STRING_MONGODB;
 
 // init express
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
+
+// init server and io
+const server = http.Server(app);
+const io = socketIo(server);
 
 // connect to mongodb + register api calls
 const MongoClient = require("mongodb").MongoClient;
@@ -24,6 +32,17 @@ MongoClient.connect(connectionString, {
     console.log("connected to mongo");
     const db = client.db("digitalresto");
     const ordersCollection = db.collection("orders");
+
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
+
+    io.on("connection", (socket) => {
+      socket.emit("news", { hello: "world" });
+      socket.on("my other event", (data) => {
+        console.log(data);
+      });
+    });
 
     app.post("/order", (req, res) => {
       postOrder(ordersCollection, req, res);
@@ -39,6 +58,6 @@ MongoClient.connect(connectionString, {
     console.error(error);
   });
 
-app.listen(port, function () {
+server.listen(port, function () {
   console.log("listening on " + port);
 });
